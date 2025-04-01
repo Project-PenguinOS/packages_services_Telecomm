0079dd119218ca03b674e92878f76e60131d0279
Grant Menke <grantmenke@google.com>
2025-04-01 12:05:29 -0700

    Guard putParcelableArrayList with flag.
    This CL guards the update to use putParcelableArrayList with the appropriate flag.
    
    Test: atest CallsManagerTest
    Bug: 307786404
    Flag: com.android.server.telecom.flags.resolve_hidden_dependencies_two
    Change-Id: I774493301bfe1e983361938e3988d8facd5ec30c

--- src/com/android/server/telecom/CallsManager.java
+++ src/com/android/server/telecom/CallsManager.java
@@ -2429,19 +2429,34 @@ public class CallsManager extends Call.ListenerBase
                                     "needs account selection");
                             // Create our own instance to modify (since extras may be Bundle.EMPTY)
                             Bundle newExtras = new Bundle(extras);
-                            ArrayList<PhoneAccountHandle> accountsFromSuggestions =
-                                    accountSuggestions
-                                    .stream()
-                                    .map(PhoneAccountSuggestion::getPhoneAccountHandle)
-                                    .collect(Collectors.toCollection(ArrayList::new));
-                            newExtras.putParcelableArrayList(
-                                    android.telecom.Call.AVAILABLE_PHONE_ACCOUNTS,
-                                    accountsFromSuggestions);
-                            ArrayList<PhoneAccountSuggestion> accountSuggestionArrayList =
-                                    new ArrayList<>(accountSuggestions);
-                            newExtras.putParcelableArrayList(
-                                    android.telecom.Call.EXTRA_SUGGESTED_PHONE_ACCOUNTS,
-                                    accountSuggestionArrayList);
+                            if (mFeatureFlags.resolveHiddenDependenciesTwo()) {
+                                ArrayList<PhoneAccountHandle> accountsFromSuggestions =
+                                        accountSuggestions
+                                                .stream()
+                                                .map(PhoneAccountSuggestion::getPhoneAccountHandle)
+                                                .collect(Collectors.toCollection(ArrayList::new));
+                                newExtras.putParcelableArrayList(
+                                        android.telecom.Call.AVAILABLE_PHONE_ACCOUNTS,
+                                        accountsFromSuggestions);
+                                ArrayList<PhoneAccountSuggestion> accountSuggestionArrayList =
+                                        new ArrayList<>(accountSuggestions);
+                                newExtras.putParcelableArrayList(
+                                        android.telecom.Call.EXTRA_SUGGESTED_PHONE_ACCOUNTS,
+                                        accountSuggestionArrayList);
+                            } else {
+                                // Legacy path:
+                                List<PhoneAccountHandle> accountsFromSuggestions =
+                                        accountSuggestions
+                                        .stream()
+                                        .map(PhoneAccountSuggestion::getPhoneAccountHandle)
+                                        .collect(Collectors.toList());
+                                newExtras.putParcelableList(
+                                        android.telecom.Call.AVAILABLE_PHONE_ACCOUNTS,
+                                        accountsFromSuggestions);
+                                newExtras.putParcelableList(
+                                        android.telecom.Call.EXTRA_SUGGESTED_PHONE_ACCOUNTS,
+                                        accountSuggestions);
+                            }
                             // Set a future in place so that we can proceed once the dialer replies.
                             mPendingAccountSelection.put(callToPlace.getId(),
                                     new CompletableFuture<>());
