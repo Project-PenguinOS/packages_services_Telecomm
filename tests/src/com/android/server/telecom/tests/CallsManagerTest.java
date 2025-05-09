3de102c4cc2c13153fd0bfaaaba3f477234f7833
Grant Menke <grantmenke@google.com>
2025-05-09 14:54:50 -0700

    Remove reference to getDefaultStreamVolume.
    This Telecom mainline cleanup CL removes the reference to getDefaultStreamVolume and replaces it with the average of the min and max stream volumes per AudioManager.
    
    Test: atest CallsManagerTest
    Bug: 307792317
    Flag: com.android.server.telecom.flags.resolve_hidden_dependencies_two
    Change-Id: If3dd114890a35735e8a92c20e391cded99341af6

--- tests/src/com/android/server/telecom/tests/CallsManagerTest.java
+++ tests/src/com/android/server/telecom/tests/CallsManagerTest.java
@@ -3429,6 +3429,7 @@ public class CallsManagerTest extends TelecomTestCase {
         verify(mComponentContextFixture.getAudioManager(), times(1)).setStreamVolume(
                 eq(AudioManager.STREAM_VOICE_CALL), anyInt(), anyInt());
     }
+
     @MediumTest
     @Test
     public void testSetCallDialingAndIncreaseVolume() {
@@ -3442,6 +3443,26 @@ public class CallsManagerTest extends TelecomTestCase {
         // We set the volume to zero above, so expect 2
         verify(mComponentContextFixture.getAudioManager(), times(2)).setStreamVolume(
                 eq(AudioManager.STREAM_VOICE_CALL), anyInt(), anyInt());
+    }
+
+    @MediumTest
+    @Test
+    public void testSetCallDialingAndCalculateAverageVolume() {
+        // This test specificaslly tests the new behavior guarded by this flag:
+        when(mFeatureFlags.resolveHiddenDependenciesTwo()).thenReturn(true);
+
+        // Start with a zero volume stream.
+        mComponentContextFixture.getAudioManager().setStreamVolume(AudioManager.STREAM_VOICE_CALL,
+                0, 0 /* flags */);
+
+        Call call = mock(Call.class);
+        mCallsManager.markCallAsDialing(call);
+
+        // Ensure we calculate the new volume using the average of AudioManager min and max volume:
+        verify(mComponentContextFixture.getAudioManager(), times(1))
+                .getStreamMaxVolume(eq(AudioManager.STREAM_VOICE_CALL));
+        verify(mComponentContextFixture.getAudioManager(), times(1))
+                .getStreamMinVolume(eq(AudioManager.STREAM_VOICE_CALL));
     }
 
     @MediumTest
