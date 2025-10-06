6dfde29642977503764b65ea6d5fbfedcbc56ee0
Liana Kazanova (xWF) <lkazanova@google.com>
2025-10-06 12:52:00 -0700

    Revert "Change default for holding calls during emergency."
    Revert submission 36112895-eccDefaultSupportHold
    
    Reason for revert: DroidMonitor: Potential culprit for http://b/449764970 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Fix: 449764970
    
    Reverted changes: /q/submissionid:36112895-eccDefaultSupportHold
    
    Change-Id: I52e8c8cce22b63171fd8b07525f0f1f6813af0d3

--- src/com/android/server/telecom/callsequencing/CallSequencingController.java
+++ src/com/android/server/telecom/callsequencing/CallSequencingController.java
@@ -1113,7 +1113,7 @@ public class CallSequencingController {
      */
     private boolean shouldHoldForEmergencyCall(PhoneAccountHandle handle) {
         return mCallsManager.getCarrierConfigForPhoneAccount(handle).getBoolean(
-                CarrierConfigManager.KEY_ALLOW_HOLD_CALL_DURING_EMERGENCY_BOOL, false);
+                CarrierConfigManager.KEY_ALLOW_HOLD_CALL_DURING_EMERGENCY_BOOL, true);
     }
 
     public static boolean arePhoneAccountsSame(Call call1, Call call2) {
