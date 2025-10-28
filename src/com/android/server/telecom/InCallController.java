66e9fcbe1c41d2ca005efb1110250d5beb8f9297
Hui Wang <huiwang@google.com>
2025-10-28 18:46:39 +0000

    Notify available call endpoints on InCall service connected
    Flag: com.android.server.telecom.flags.notify_available_endpoints_on_ics_connected
    Bug: 448784549
    Test: atest TelecomUnitTests:InCallControllerTests
    Change-Id: Ie7705fcbe9624827a9a930df32a10caa18957900

--- src/com/android/server/telecom/InCallController.java
+++ src/com/android/server/telecom/InCallController.java
@@ -2849,6 +2849,10 @@ public class InCallController extends CallsManagerListenerBase implements
         try {
             inCallService.onCallAudioStateChanged(mCallsManager.getAudioState());
             inCallService.onCanAddCallChanged(mCallsManager.canAddCall());
+            if (mFeatureFlags.notifyAvailableEndpointsOnIcsConnected()) {
+                inCallService.onAvailableCallEndpointsChanged(new ArrayList<>(
+                        mCallsManager.getCallEndpointController().getAvailableEndpoints()));
+            }
             inCallService.onCallEndpointChanged(mCallsManager.getCallEndpointController()
                     .getCurrentCallEndpoint());
         } catch (RemoteException ignored) {
