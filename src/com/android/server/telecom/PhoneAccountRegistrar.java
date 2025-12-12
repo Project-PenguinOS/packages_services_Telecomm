8a66f8e610e598c1bdb1b65689b97d16f98228a2
Arunraj M <arunrajms@google.com>
2025-12-12 15:51:14 +0000

    Remove PhoneAccountRegistrar dependencies on UserInfo
    Bug: 443131372
    Test: Build with and without release flag. Presubmit
    Flag: build.RELEASE_TELECOM_MAINLINE_MODULE
    Change-Id: I98ebe95074be49b72f7e48cd58029775c284ab53

--- src/com/android/server/telecom/PhoneAccountRegistrar.java
+++ src/com/android/server/telecom/PhoneAccountRegistrar.java
@@ -2780,12 +2780,6 @@ public class PhoneAccountRegistrar {
                             // UserManager#getMainUser requires either the MANAGE_USERS,
                             // CREATE_USERS, or QUERY_USERS permission.
                             UserHandle primaryUser = userManager.getMainUser();
-                            UserInfo primaryUserInfo = userManager.getPrimaryUser();
-                            if (!telecomFeatureFlags.telecomResolveHiddenDependencies()) {
-                                primaryUser = primaryUserInfo != null
-                                        ? primaryUserInfo.getUserHandle()
-                                        : null;
-                            }
                             if (primaryUser != null) {
                                 DefaultPhoneAccountHandle defaultPhoneAccountHandle
                                         = new DefaultPhoneAccountHandle(primaryUser,
